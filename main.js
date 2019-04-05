8526be631516029447c8e671a765d682546c162a
Jordan Huus <jordanhuusy@live.com>
2019-04-04 17:52:27 -0700

    cleaning code format

--- main.js
+++ main.js
@@ -29,36 +29,40 @@ var nodes = d3.hierarchy(treeData);
 nodes = treemap(nodes);
 
 // append the svg obgect to the body of the page
-// appends a 'group' element to 'svg'
-// moves the 'group' element to the top left margin
 var svg = d3.select("body").append("svg")
       .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom),
-    g = svg.append("g")
-      .attr("transform",
-            "translate(" + margin.left + "," + margin.top + ")");
+      .attr("height", height + margin.top + margin.bottom);
+
+// appends a 'group' element to 'svg'
+// moves the 'group' element to the top left margin
+var g = svg.append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 // adds the links between the nodes
 var link = g.selectAll(".link")
-    .data( nodes.descendants().slice(1))
-  .enter().append("path")
-    .attr("class", "link")
-    .attr("d", function(d) {
-       return "M" + d.x + "," + d.y
-         + "C" + d.x + "," + (d.y + d.parent.y) / 2
-         + " " + d.parent.x + "," +  (d.y + d.parent.y) / 2
-         + " " + d.parent.x + "," + d.parent.y;
-       });
+    .data(nodes.descendants().slice(1))
+    .enter()
+    .append("path")
+        .attr("class", "link")
+        .attr("d", function(d) {
+            return "M" + d.x + "," + d.y
+            + "C" + d.x + "," + (d.y + d.parent.y) / 2
+            + " " + d.parent.x + "," +  (d.y + d.parent.y) / 2
+            + " " + d.parent.x + "," + d.parent.y;
+        });
 
 // adds each node as a group
 var node = g.selectAll(".node")
     .data(nodes.descendants())
-  .enter().append("g")
-    .attr("class", function(d) {
-      return "node" +
-        (d.children ? " node--internal" : " node--leaf"); })
-    .attr("transform", function(d) {
-      return "translate(" + d.x + "," + d.y + ")"; });
+    .enter()
+    .append("g")
+        .attr("class", function(d) {
+            return "node" +
+            (d.children ? " node--internal" : " node--leaf");
+        })
+        .attr("transform", function(d) {
+            return "translate(" + d.x + "," + d.y + ")";
+        });
 
 // adds the circle to the node
 node.append("circle")
@@ -70,3 +74,21 @@ node.append("text")
   .attr("y", function(d) { return d.children ? -20 : 20; })
   .style("text-anchor", "middle")
   .text(function(d) { return d.data.name; });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
