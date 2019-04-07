862246d31a3da2c7b03a5ff0498be2454d3857fe
Jordan Huus <jordanhuusy@live.com>
2019-04-06 22:49:49 -0700

    adding class to set node locations; methods getDirectionalNodesGraph(), getNodeById(), and getNodeSize()

---
+++ DirectionalNodeGraph.js
@@ -0,0 +1,83 @@
+var treeData = [
+    {
+        "name": "Node 1",
+        "id": 1,
+        "parents": []
+    },
+    {
+        "name": "Node 2",
+        "id": 2,
+        "parents": []
+    },
+    {
+        "name": "Node 3",
+        "id": 3,
+        "parents": [1,2]
+    }
+];
+
+
+class DirectionalNodeGraph{
+    constructor(data){
+        this.nodes = data;
+    }
+
+    // Sets x,y locations for all nodes
+    getDirectionalNodesGraph(){
+        for (var i = 0; i < this.nodes.length; i++) {
+            if(this.nodes[i].parents){
+                nodeHeight = 0;
+                nodeWidth = 0;
+                for (var x = 0; x < this.nodes.length; x++) {
+                    var nodeSize = getNodeSize(getNodeById(this.nodes[x].id));
+                    nodeHeight += nodeSize[0];
+                    nodeWidth += nodeSize[1];
+                }
+            }
+        }
+    }
+
+
+    getNodeById(id){
+        for (var i = 0; i < this.nodes.length; i++) {
+            if(this.nodes[i].id===id){
+                return this.nodes[i];
+            }
+        }
+    }
+
+
+    // Recursive fuction to get node parent size
+    getNodeSize(node){
+        // Node doesn't have parents, return height 1, width 1
+        if(!node.parents){return [1,1];}
+
+
+        var nodeHeight = 0;
+        var nodeWidth = 0;
+        for (var i = 0; i < node.parents.length; i++) {
+            if(node.parents){
+                for (var x = 0; x < this.nodes.length; x++) {
+                    var nodeSize = getNodeSize(getNodeById(this.nodes[x].id));
+                    nodeHeight += nodeSize[0];
+                    nodeWidth += nodeSize[1];
+                }
+            }
+        }
+    }
+
+
+}
+
+
+let directionalNodeGraph = new DirectionalNodeGraph(treeData);
+console.log(directionalNodeGraph.getNodesLength());
+
+
+
+
+
+
+
+
+//
