b17da0c5073f77082b64215abd94c46045f67175
Jordan Huus <jordanhuusy@live.com>
2019-04-10 23:02:20 -0700

    working to recursively calculate node widths; using getPathsCountBetweenTwoNodes to determine 'immediate' parent nodes.

--- DirectionalNodeGraph.js
+++ DirectionalNodeGraph.js
@@ -21,23 +21,15 @@ var treeData = [
 class DirectionalNodeGraph{
     constructor(data){
         this.nodes = data;
+        this.nodes.forEach(function(node){
+            node.visited = false;
+        });
     }
 
-    // Sets x,y locations for all nodes
-    getDirectionalNodesGraph(){
-        for (var i = 0; i < this.nodes.length; i++) {
-            if(this.nodes[i].parents.length > 0){
-                var nodeHeight = 0;
-                var nodeWidth = 0;
-                for (var x = 0; x < this.nodes[i].parents.length; x++) {
-                    var nodeSize = this.getNodeSize(this.getNodeById(this.nodes[i].parents[x]));
-                    nodeHeight += nodeSize[0];
-                    nodeWidth += nodeSize[1];
-                }
-            }
-        }
-    }
 
+    getDirectionalNodeGraph(){
+        setNodeHeightAndWidth
+    }
 
     getNodeById(id){
         for (var i = 0; i < this.nodes.length; i++) {
@@ -47,29 +39,85 @@ class DirectionalNodeGraph{
         }
     }
 
+    // Calculates the width of each node based on the number parent nodes
+    setNodeHeightAndWidth(){
+        for (var i = 0; i < this.nodes.length; i++) {
+            if(this.nodes[i].visited === false){
 
-    // Recursive fuction to get node parent size
-    getNodeSize(node){
-        // Node doesn't have parents, return height 1, width 1
-        if(!node.parents){return [1,1];}
+                if(this.nodes[i].parents > 0){
+                    this.nodes[i].
+                }
+
+                var nodeWidth = 0;
+
+                for (var x = 0; x < this.nodes[i].parents.length; x++) {
+                    nodeWidth += this.getNodeWidth(this.getNodeById(this.nodes[i].parents[x]));
+                }
 
 
-        // Calculate each node height and width
-        var nodeHeight = 0;
-        var nodeWidth = 0;
-        for (var i = 0; i < node.parents.length; i++) {
-            for (var x = 0; x < this.nodes.length; x++) {
-                var nodeSize = this.getNodeSize(this.getNodeById(this.nodes[x].id));
-                nodeHeight += nodeSize[0];
-                nodeWidth += nodeSize[1];
+                this.nodes[i].height = nodeHeight;
+                this.nodes[i].width = nodeWidth;
             }
         }
     }
+
+
+    // Calculate widths for all nodes
+    getNodeWidth(node){
+        // Node already visited, skip
+        if(node.visited){return [node.height, node.width];}
+
+        // Leaf node; width = 1
+        if(node.parents.length===0){
+            node.visited = true;
+            return 1;
+        }
+
+        var width = 0;
+
+        // Add all parent node widths
+        for (var i = 0; i < node.parents.length; i++) {
+            // Skip parents with more than one path to root
+            if(this.getPathsCountBetweenTwoNodes(node, this.getNodeById(node.parents[i]))){continue;}
+
+            nodeWidth += this.getNodeWidth(this.getNodeById(node.parents[i]));
+        }
+
+        node.visited = true;
+    }
+
+
+    // DFS to count the number of paths available between a node and one of it's parents
+    getPathsCountBetweenTwoNodes(rootNode, targetNode){
+
+        var visitedNodes = [];
+
+        for (var i = 0; i < rootNode.parents.length; i++) {
+            visitedNodes.push(rootNode.parents[i]);
+            if(isPath(this.getNodeById(rootNode.parents[i]), targetNode, visitedNodes)){
+                return true;
+            }
+        }
+    }
+    isPath(rootNode, targetNode, visitedNodes){
+
+        if(rootNode.id === targetNode.id){return true;}
+
+        for (var i = 0; i < rootNode.parents.length; i++) {
+            if(isPath(this.getNodeById(rootNode.parents[i]), targetNode, visitedNodes)){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+
 }
 
 
 let directionalNodeGraph = new DirectionalNodeGraph(treeData);
-console.log(directionalNodeGraph.getDirectionalNodesGraph());
 
 
 
